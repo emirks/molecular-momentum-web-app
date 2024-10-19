b7dc28bceab1704bf4499e2134fe46cc6c3f4f95
emirks <emirks88@gmail.com>
2024-10-19 20:10:37 +0300

    [feat] Add duckdns to django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'yourapp.duckdns.org']
+ALLOWED_HOSTS = ['localhost', 'molecularmomentum.duckdns.org']
 
 
 # Application definition
